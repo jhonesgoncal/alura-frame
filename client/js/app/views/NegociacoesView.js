f72ff6d40bde3a57e102e1108efab1357cd2c805
SQZ69 <SQZ69@LAPTOP-6DLUCOML>
2017-08-23 17:52:41 -0300

    implementando reduce

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -31,6 +31,8 @@ class NegociacoesView{
                 </tbody>
                 
                 <tfoot>
+                    <td colspan=3></td>
+                    <td>${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}</td>
                 </tfoot>
             </table>
         `;
