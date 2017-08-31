ec557308fc1f7d299a549b91c53a95c9f8949d3c
SQZ69 <SQZ69@LAPTOP-6DLUCOML>
2017-08-31 17:51:38 -0300

    add mensagem

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,16 +5,22 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model=>
+            this._negociacoesView.update(model)
+        );
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
+        
     }
 
     adiciona(event){
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = "Negociação adicionada com sucesso!";
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
@@ -31,6 +37,12 @@ class NegociacaoController{
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();
+    }
+
+    apaga(){
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem)
     }
 }
 
